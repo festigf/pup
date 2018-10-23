c8a86e1ea798f374c13da3268313af37ef71dccc
festigf <gianfranco.festi@marconirovereto.it>
2018-10-23 17:14:05 +0200

    funziona con username e pwd fisso

--- app.js
+++ app.js
@@ -1,41 +1,26 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: false,executablePath: 'Chrome'});
   const page = await browser.newPage();
   await page.goto('https://pf.fondimpresa.it/fpf/index.jsp?parameter=weak');
-  //await page.screenshot({path: 'example.png'});
 
-   ////page.click("username"); //Focus on input element
-   //page.type("gigi");
-   await page.type('username', 'test');
+   await page.waitFor('input[name=userName]');
+   await page.$eval('input[name=userName]', el => el.value = 'gcherubino');
+   await page.waitFor('input[name=password]');
+   await page.$eval('input[name=password]', el => el.value = '000075610');
 
+	let selector = 'input';
+	await page.$$eval(selector, anchors => {
+		anchors.map(anchor => {
+			console.log(anchor.value );
+			if(anchor.value == 'LOGIN') {
+				anchor.click();
+				return
+			}
+		})
+	});
+  
+   
 //  await browser.close();
 })();
-
-
-
-/*
-    ie.Document.getElementsByName("userName")(0).Value = userName
-    ie.Document.getElementsByName("password")(0).Value = Password
-    Set HTMLDoc = ie.Document
-    For Each oHTML_Element In HTMLDoc.querySelectorAll("input")
-        If oHTML_Element.Value = "LOGIN" Then
-            oHTML_Element.Click
-
-*/
-
-/**
- * Set value on a select element
- * @param {string} value
- * @returns {Promise<Undefined>}
-
-ElementHandle.prototype.select = async function( value ) {
-  await this._page.evaluateHandle( ( el, value ) => {
-      const event = new Event( "change", { bubbles: true });
-      event.simulated = true;
-      el.querySelector( `option[value="${ value }"]` ).selected = true;
-      el.dispatchEvent( event );
-  }, this, value );
-};
-*/
