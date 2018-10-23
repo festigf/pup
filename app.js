93af43aaf36a66d99fa4b9c4da2c6fbe09f2bbf7
festifg <gianfrancgo.festi@marconirovereto.it>
2018-10-23 21:47:52 +0200

    versione ok con feedback dopo click login

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //http://localhost:3000/openfi?username=gcherubino&password=000075610
 const puppeteer = require('puppeteer');
 const express=require("express");
-//var bodyParser= require("body-parser");
+var bodyParser= require("body-parser");
+const util = require('util')
 var app=express();
 //app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/openfi",function(req,res)
 {
@@ -11,9 +13,12 @@ app.get("/openfi",function(req,res)
 	var pwd=req.query.password;
 	console.log(username + " - " + pwd);
 	openFI(username,pwd)
-		.then((status) =>{
-			res.status(status);
-			res.end("fatto");
+		.then((ret) =>{
+			console.log(ret) ;
+			console.log(util.inspect(ret, false, null, true )) 
+			//var jsonRes = JSON.parse(ret);
+			res.status(200);
+			res.end(ret);
 		})
 })
 
@@ -43,36 +48,31 @@ async function openFI(username,pwd){
 				  }
 			  })
 		  });
-		  page.waitForNavigation({waitUntil: 'load'});		
-		 /* //<i>Attenzione, si è verificato un errore</i>
-		  await page.waitFor('input[name=userName]');
-		  await page.$eval('input[name=userName]', (el,_username) => el.value = _username,username); */
-		  console.log("fine")
-		  return 201
+		  page.waitForNavigation({waitUntil: 'load'});	
+		  selector = 'i';
+
+//		  const innerText = await page.evaluate(() => document.querySelector('i').innerText);
+//		  console.log(innerText);
+		  const textsArray = await  page.evaluate(
+			() => [...document.querySelectorAll('i')].map(elem => {
+				 if ( elem.innerText.indexOf("errore")!=-1) elem.innerText
+				})
+		  );
+		  console.log(textsArray);
+
+//		  console.log(textsArrayb);
+		  //<i>Attenzione, si è verificato un errore</i>
+		  if (textsArray.length>0 ){
+			const textsArrayb =  await page.evaluate(
+				() => [...document.querySelectorAll('b')].map(elem => elem.innerText)
+			);
+			return '{"Status":201,"msg":"'+textsArrayb[0]+'"}';
+		  }
+		  else
+		  {
+				console.log("fine")
+				return '{"Status":200,"msg":"Ok"}';
+		  }
+
  }
 
-
-/*(async () => {
-  const browser = await puppeteer.launch({headless: false,executablePath: 'Chrome'});
-  const page = await browser.newPage();
-  await page.goto('https://pf.fondimpresa.it/fpf/index.jsp?parameter=weak');
-
-   await page.waitFor('input[name=userName]');
-   await page.$eval('input[name=userName]', el => el.value = 'gcherubino');
-   await page.waitFor('input[name=password]');
-   await page.$eval('input[name=password]', el => el.value = '000075610');
-
-	let selector = 'input';
-	await page.$$eval(selector, anchors => {
-		anchors.map(anchor => {
-			console.log(anchor.value );
-			if(anchor.value == 'LOGIN') {
-				anchor.click();
-				return
-			}
-		})
-	});
-  
-   
-
-})();*/
