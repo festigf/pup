a3dab40e20e6d8f204ac33ed4f48de05acba29a5
festifg <gianfrancgo.festi@marconirovereto.it>
2018-10-26 21:09:24 +0200

    path chrome with regedit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 //http://localhost:3000/openfi?username=gcherubino&password=000075610
 const puppeteer = require('puppeteer');
+//var key = require('windows-registry').Key;
+//var chromepath = key.getValue('chrome.exe');
+var chromepath ="";
 const express=require("express");
 var bodyParser= require("body-parser");
 const util = require('util')
@@ -7,10 +10,18 @@ var app=express();
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get("/SetPath",function(req,res)
+{
+	chromepath=req.query.chromepath;
+	console.log(chromepath);
+	res.end(200);
+});
+
 app.get("/openfi",function(req,res)
 {
 	var username=req.query.username;
 	var pwd=req.query.password;
+	console.log(chromepath);
 	console.log(username + " - " + pwd);
 	openFI(username,pwd)
 		.then((ret) =>{
@@ -20,6 +31,7 @@ app.get("/openfi",function(req,res)
 			res.status(200);
 			res.end(ret);
 		})
+	
 })
 
 
@@ -28,8 +40,8 @@ app.listen(3000);
 
 async function openFI(username,pwd){
 		//console.log(username + " - " + pwd);
-	
-		const browser = await puppeteer.launch({headless: false,executablePath: 'Chrome'});
+		
+		const browser = await puppeteer.launch({headless: false,executablePath: chromepath+"\\chrome"});
 		const page = await browser.newPage();
 		await page.goto('https://pf.fondimpresa.it/fpf/index.jsp?parameter=weak');
 	  
